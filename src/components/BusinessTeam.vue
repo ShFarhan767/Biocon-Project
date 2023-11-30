25e457da2aca4b0c1a5de17a13a844c73df82c67
ShFarhan767 <farhan@doofazit.com>
2023-11-30 12:31:47 +0600

    done

--- src/components/BusinessTeam.vue
+++ src/components/BusinessTeam.vue
@@ -69,5 +69,16 @@
 <style scoped>
 .fh5co-cta{
 	background-image: url(../../image/business/business-footer-bg.jpeg);
+	height: 270px;
+}
+.fh5co-cta .overlay {
+    background: rgba(0, 0, 0, 0.7);
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    position: absolute;
+    z-index: 1;
+    height: 270px;
 }
 </style>
